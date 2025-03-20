e585058c32f430fd0582708eb90e4b608e92f693
Nitish jhamb <nitishjhamb20@gmail.com>
2025-03-20 12:41:36 +0530

    Resume Updated

--- src/components/Resume.tsx
+++ src/components/Resume.tsx
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from "react";
-import "../assets/styles/Resume.scss"; 
+import "../assets/styles/Resume.scss";
 
 const Resume: React.FC = () => {
   const googleDrivePdfUrl =
-    "https://drive.google.com/file/d/1-dQfAfi2a_UrnjiWXA7PeLbt4Y8kpD8F/preview";
+    "https://drive.google.com/file/d/1HfBEHKRJAeqYvn6mCnplxJ7dkKlKWMDN/preview";
   const googleDriveDownloadUrl =
-    "https://drive.google.com/uc?export=download&id=1-dQfAfi2a_UrnjiWXA7PeLbt4Y8kpD8F";
+    "https://drive.google.com/uc?export=download&id=1HfBEHKRJAeqYvn6mCnplxJ7dkKlKWMDN";
 
   const [isMobile, setIsMobile] = useState<boolean>(window.innerWidth < 768);
 
