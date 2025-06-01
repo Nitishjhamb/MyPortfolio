4b67b4e34d475bbd2bd07f35ead635920b526b8d
Nitish jhamb <nitishjhamb20@gmail.com>
2025-06-01 16:21:02 +0530

    timeline and bg image updations

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -1,58 +1,84 @@
 import React from "react";
-import '@fortawesome/free-regular-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGraduationCap } from '@fortawesome/free-solid-svg-icons';
-import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
-import 'react-vertical-timeline-component/style.min.css';
-import '../assets/styles/Timeline.scss';
+import "@fortawesome/free-regular-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGraduationCap } from "@fortawesome/free-solid-svg-icons";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
+import "../assets/styles/Timeline.scss";
 
 function Timeline() {
   return (
     <div id="education">
       <div className="items-container">
         <h1>Education</h1>
+
         <VerticalTimeline>
+          {/* 2023-2025 --------------------------------------------------- */}
           <VerticalTimelineElement
             className="vertical-timeline-element--education"
-            contentStyle={{ background: 'white', color: 'rgb(39, 40, 34)' }}
-            contentArrowStyle={{ borderRight: '7px solid  white' }}
+            contentArrowStyle={{ borderRight: "7px solid  white" }}
             date="2023 - 2025"
-            iconStyle={{ background: '#5000ca', color: 'rgb(39, 40, 34)' }}
+            iconStyle={{ background: "#5000ca", color: "rgb(39, 40, 34)" }}
             icon={<FontAwesomeIcon icon={faGraduationCap} />}
           >
-            <h3 className="vertical-timeline-element-title">Master in Computer Application</h3>
-            <h4 className="vertical-timeline-element-subtitle">Chandigarh University</h4>
+            <h3 className="vertical-timeline-element-title">
+              Master in Computer Application
+            </h3>
+            <h4 className="vertical-timeline-element-subtitle">
+              Chandigarh University
+            </h4>
             <p>Specialization in Cloud Computing & DevOps</p>
           </VerticalTimelineElement>
 
+          {/* 2019-2022 --------------------------------------------------- */}
           <VerticalTimelineElement
             className="vertical-timeline-element--education"
+            contentArrowStyle={{ borderRight: "7px solid  white" }}
             date="2019 - 2022"
-            iconStyle={{ background: '#5000ca', color: 'rgb(39, 40, 34)' }}
+            iconStyle={{ background: "#5000ca", color: "rgb(39, 40, 34)" }}
             icon={<FontAwesomeIcon icon={faGraduationCap} />}
           >
-            <h3 className="vertical-timeline-element-title">Bachelors in Computer Application</h3>
-            <h4 className="vertical-timeline-element-subtitle">Tirupati College of Technical Education, Jaipur, Rajasthan</h4>
+            <h3 className="vertical-timeline-element-title">
+              Bachelors in Computer Application
+            </h3>
+            <h4 className="vertical-timeline-element-subtitle">
+              Tirupati College of Technical Education, Jaipur, Rajasthan
+            </h4>
           </VerticalTimelineElement>
 
+          {/* 2017-2019 --------------------------------------------------- */}
           <VerticalTimelineElement
             className="vertical-timeline-element--education"
+            contentArrowStyle={{ borderRight: "7px solid  white" }}
             date="2017 - 2019"
-            iconStyle={{ background: '#5000ca', color: 'rgb(39, 40, 34)' }}
+            iconStyle={{ background: "#5000ca", color: "rgb(39, 40, 34)" }}
             icon={<FontAwesomeIcon icon={faGraduationCap} />}
           >
-            <h3 className="vertical-timeline-element-title">Intermediate (CBSE)</h3>
-            <h4 className="vertical-timeline-element-subtitle">Step by Step Public School, Alwar, Rajasthan</h4>
+            <h3 className="vertical-timeline-element-title">
+              Intermediate (CBSE)
+            </h3>
+            <h4 className="vertical-timeline-element-subtitle">
+              Step by Step Public School, Alwar, Rajasthan
+            </h4>
           </VerticalTimelineElement>
 
+          {/* 2016-2017 --------------------------------------------------- */}
           <VerticalTimelineElement
             className="vertical-timeline-element--education"
+            contentArrowStyle={{ borderRight: "7px solid  white" }}
             date="2016 - 2017"
-            iconStyle={{ background: '#5000ca', color: 'rgb(39, 40, 34)' }}
+            iconStyle={{ background: "#5000ca", color: "rgb(39, 40, 34)" }}
             icon={<FontAwesomeIcon icon={faGraduationCap} />}
           >
-            <h3 className="vertical-timeline-element-title">Matriculation (RBSE)</h3>
-            <h4 className="vertical-timeline-element-subtitle">Sanskar Valley Public School, Ramgarh, Alwar</h4>
+            <h3 className="vertical-timeline-element-title">
+              Matriculation (RBSE)
+            </h3>
+            <h4 className="vertical-timeline-element-subtitle">
+              Sanskar Valley Public School, Ramgarh, Alwar
+            </h4>
           </VerticalTimelineElement>
         </VerticalTimeline>
       </div>
